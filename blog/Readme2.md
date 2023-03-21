a4cd0dd0a30c6bdc104d5dfce858c81b26b395b9
mrzshun <zmarias@loginet.hu>
2023-03-21 10:35:33 +0100

    3. gyakorlat anyaga

---
+++ blog/Readme2.md
@@ -0,0 +1,19 @@
+Hasznos linkek:
+https://laravel.com/docs/10.x
+https://laravel.com/docs/10.x/eloquent
+https://laravel.com/docs/10.x/collections
+
+CRUD:
+Create, Read, Update, Delete
+
+Parancsok:
+php artisan make:model Flight
+php artisan make:model Flight -mf
+létrehozza a modelt, és hozzá a migrationt és a factory-t
+
+Blog projekt
+
+Van benne 3 (2+1) entitás
+Category: name, background color, text color
+Post: title, text, description, author
+Author (beépített User modelt fogjuk használni)
---
+++ blog/database/factories/CategoryFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'txt_color' => fake()->safeHexColor().'ff',
+            'bg_color' => fake()->safeHexColor().'ff',
+        ];
+        // return [
+        //     'name' => fake()->name(),
+        //     'email' => fake()->unique()->safeEmail(),
+        //     'email_verified_at' => now(),
+        //     'password' =>  '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //     'remember_token' => Str::random(10),
+        // ];
+
+        // $table->string('name');
+        // $table->string('txt_color',9);
+        // $table->string('bg_color',9);
+
+    }
+}
